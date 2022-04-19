4cf6c96802e8d09ff151f59c9e1e2ac7d9dc0fa5
Sayee Bhoite <sayeebhoite7@gmail.com>
2022-04-19 13:35:19 +0530

    Fe-Be

---
+++ facedoc/users/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from . import models
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+class UserAdmin(BaseUserAdmin):
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('id', 'email', 'fname', 'mname', 'lname', 'phone', 'role', 'tc', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('fname', 'mname', 'lname', 'phone','role', 'tc')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'fname', 'mname', 'lname', 'phone', 'role', 'tc', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email','id')
+    filter_horizontal = ()
+
+
+# Now register the new UserAdmin...
+admin.site.register(models.UserData, UserAdmin)
+admin.site.register(models.RoleAssigned)
---
+++ facedoc/users/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import UserData, RoleAssigned
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+
+    password2=serializers.CharField(style={'input_type':'password'}, write_only=True)
+    class Meta:
+        model=UserData
+        fields=['email','fname','mname','lname','phone','role','tc','password','password2']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+
+        # validation password and confirm password
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm Password does not match.")
+        return attrs
+
+    def create(self,validate_data):
+        return UserData.objects.create_user(**validate_data)
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email=serializers.EmailField(max_length=255)
+    class Meta:
+        model=UserData
+        fields=['email','password']
+
+class UserProfileSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = UserData
+    fields = ['id', 'email', 'fname','mname','lname','phone','role']
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoleAssigned
+        fields = ['name']
