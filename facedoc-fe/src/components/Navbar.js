4b5d00e7d50b5af33f71faef72664aa6381f2b3f
Sumeet Balwade <balwadesr@gmail.com>
2022-05-05 20:12:08 +0530

    my changes

--- facedoc-fe/src/components/Navbar.js
+++ facedoc-fe/src/components/Navbar.js
@@ -1,120 +1,149 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
-import axiosInstance from '../axios';
-
+import { react, useEffect, useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import axiosInstance from "../axios";
+import axios from "axios";
+import { TextField } from "@mui/material/TextField";
 
 export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const history = useHistory();
-  const logout = () => {
-      axiosInstance.post('logout/', {
-        refresh_token: localStorage.getItem('refresh_token'),
-      });
-      localStorage.removeItem('access_token');
-      localStorage.removeItem('refresh_token');
-      axiosInstance.defaults.headers['Authorization'] = null;
-      history.push('/signin');
-  };
+	const [auth, setAuth] = useState(true);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const history = useHistory();
+	const logout = () => {
+		axiosInstance.post("logout/", {
+			refresh_token: localStorage.getItem("refresh_token"),
+		});
+		localStorage.removeItem("access_token");
+		localStorage.removeItem("refresh_token");
+		axiosInstance.defaults.headers["Authorization"] = null;
+		history.push("/signin");
+	};
 
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
+	const handleChange = (event) => {
+		setAuth(event.target.checked);
+	};
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleMenu = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={auth}
-                onChange={handleChange}
-                aria-label='login switch'
-              />
-            }
-            label={auth ? 'Logout' : 'Login'}
-          />
-        </FormGroup>
-        <AppBar position='fixed' sx={{ backgroundColor: 'black' }}>
-          <Toolbar>
-            <IconButton
-              size='large'
-              edge='start'
-              color='inherit'
-              aria-label='menu'
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-              <Link
-                href='/landing'
-                style={{
-                  color: 'white',
-                  textDecoration: 'none',
-                  cursor: 'pointer',
-                }}
-              >
-                FaceDoc
-              </Link>
-            </Typography>
-            {auth && (
-              <div>
-                <IconButton
-                  size='large'
-                  aria-label='account of current user'
-                  aria-controls='menu-appbar'
-                  aria-haspopup='true'
-                  onClick={handleMenu}
-                  color='inherit'
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id='menu-appbar'
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                >
-                  <MenuItem > <Link className='home-link' to='/user'>Profile</Link> </MenuItem>
-                  <MenuItem  onClick={logout}> <Link className='home-link' to='/signin'>Logout</Link></MenuItem>
-                </Menu>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </div>
-  );
+	const [name, setName] = useState("");
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:8000/user/profile", {
+				headers: {
+					"Content-type": "application/json",
+					Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUxNzYwNTA1LCJpYXQiOjE2NTE3NjAyMDUsImp0aSI6ImNmMjdhZTE0MmEwMzQ4YWRiZDFiYmUwZDAzOGFhYTE2IiwidXNlcl9pZCI6MX0.lMXL6Y9WFv3glGr7KdI14VwWLW4_qC3EKEZf2dZOPik`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+
+				const { fname, lname } = res.data;
+				setName(fname + " " + lname);
+			})
+			.catch((err) => console.log(err));
+	}, []);
+	return (
+		<div>
+			<Box sx={{ flexGrow: 1 }}>
+				<FormGroup>
+					<FormControlLabel
+						control={
+							<Switch
+								checked={auth}
+								onChange={handleChange}
+								aria-label='login switch'
+							/>
+						}
+						label={auth ? "Logout" : "Login"}
+					/>
+				</FormGroup>
+				<AppBar position='fixed' sx={{ backgroundColor: "black" }}>
+					<Toolbar>
+						<IconButton
+							size='large'
+							edge='start'
+							color='inherit'
+							aria-label='menu'
+							sx={{ mr: 2 }}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+							<Link
+								href='/landing'
+								style={{
+									color: "white",
+									textDecoration: "none",
+									cursor: "pointer",
+								}}
+							>
+								FaceDoc
+							</Link>
+						</Typography>
+						{auth && (
+							<div>
+								<IconButton
+									size='large'
+									aria-label='account of current user'
+									aria-controls='menu-appbar'
+									aria-haspopup='true'
+									onClick={handleMenu}
+									color='inherit'
+								>
+									<AccountCircle /> <Typography padding={1}>{name}</Typography>
+								</IconButton>
+								<Menu
+									id='menu-appbar'
+									anchorEl={anchorEl}
+									anchorOrigin={{
+										vertical: "top",
+										horizontal: "right",
+									}}
+									keepMounted
+									transformOrigin={{
+										vertical: "top",
+										horizontal: "right",
+									}}
+									open={Boolean(anchorEl)}
+									onClose={handleClose}
+								>
+									<MenuItem>
+										{" "}
+										<Link className='home-link' to='/user'>
+											Profile
+										</Link>{" "}
+									</MenuItem>
+									<MenuItem onClick={logout}>
+										{" "}
+										<Link className='home-link' to='/signin'>
+											Logout
+										</Link>
+									</MenuItem>
+								</Menu>
+							</div>
+						)}
+					</Toolbar>
+				</AppBar>
+			</Box>
+		</div>
+	);
 }
--- facedoc-fe/src/page/Dashboard.js/Dashboard.js
+++ facedoc-fe/src/page/Dashboard.js/Dashboard.js
@@ -11,77 +11,64 @@ import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Navbar from "../../components/Navbar";
+import axios from "axios";
 
 const theme = createTheme();
 function Dashboard() {
-  React.useEffect(() => {
-    // console.log(localStorage.getItem('access_token'))
-    fetch("http://localhost:8000/user/profile", {
-      headers: {
-        "Content-type": "application/json",
-        "Authorization" : `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUxMDQxNzQ0LCJpYXQiOjE2NTEwNDE0NDQsImp0aSI6IjMzZGM2ZGQ3NmEyYzQ2MzI4NGVkNmU3MjY1OGMzZWEyIiwidXNlcl9pZCI6MX0.FojGlOrrkZ6vIBvMuJ27GntjoFwvtBiHBxMuTBnKgjo`
-      },
-
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        // console.log(data);
-      });
-  }, []);
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Navbar />
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="sm">
-          <Card>
-            <Card.Body>
-              <CssBaseline />
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <Typography component="h1" variant="h5">
-                  Get User Details by:
-                </Typography>
-                <Grid container spacing={2}>
-                  <Grid item xs={13}>
-                    <label htmlFor="">Enter Name</label>
-                    <TextField
-                      required
-                      fullWidth
-                      id="name"
-                      label="Name"
-                      name="name"
-                      autoComplete="name"
-                    />
-                    <Form.Group controlId="formFileLg" className="mb-3">
-                      <br />
-                      <Form.Label>Upload File</Form.Label>
-                      <Form.Control type="file" size="lg" />
-                    </Form.Group>
-                    <Button variant="dark" size="lg">
-                      Submit
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Box>
-            </Card.Body>
-          </Card>
-        </Container>
-      </ThemeProvider>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				height: "100vh",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Navbar />
+			<ThemeProvider theme={theme}>
+				<Container component='main' maxWidth='sm'>
+					<Card>
+						<Card.Body>
+							<CssBaseline />
+							<Box
+								sx={{
+									display: "flex",
+									flexDirection: "column",
+									alignItems: "center",
+								}}
+							>
+								<Typography component='h1' variant='h5'>
+									Get User Details by:
+								</Typography>
+								<Grid container spacing={2}>
+									<Grid item xs={13}>
+										<label htmlFor=''>Enter Name</label>
+										<TextField
+											required
+											fullWidth
+											id='name'
+											label='Name'
+											name='name'
+											autoComplete='name'
+										/>
+										<Form.Group controlId='formFileLg' className='mb-3'>
+											<br />
+											<Form.Label>Upload File</Form.Label>
+											<Form.Control type='file' size='lg' />
+										</Form.Group>
+										<Button variant='dark' size='lg'>
+											Submit
+										</Button>
+									</Grid>
+								</Grid>
+							</Box>
+						</Card.Body>
+					</Card>
+				</Container>
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default Dashboard;
