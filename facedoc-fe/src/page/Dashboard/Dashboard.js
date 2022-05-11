02d77ee625c654624515c28f678de376f0bc5a88
Smeet <smeetramteke21@gmail.com>
2022-05-11 13:28:07 +0530

    Smeet - Document Table Component

--- facedoc-fe/src/page/Dashboard/Dashboard.js
+++ facedoc-fe/src/page/Dashboard/Dashboard.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Typography,
-  Stack,
-  IconButton,
   Card,
   Grid,
   CardContent,
   CardActions,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
 } from '@mui/material';
-import camera from '../../assets/camera.jpg';
-import { styled } from '@mui/material/styles';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import Navbar from '../../components/Navbar';
 import './Dashboard.css';
-
-const Input = styled('input')({
-  display: 'none',
-});
+import axiosInstance from '../../axios';
+import camera from '../../assets/camera.jpg';
 
 function Dashboard() {
+  const [load, setLoad] = useState(true);
+  const [file, setFile] = useState('');
+  const [uploadedFile, setUploadedFile] = useState('');
+  const [documents, setDocuments] = useState([
+    {
+      name: 'Aadhar',
+      link: '#',
+    },
+    {
+      name: 'Driving License',
+      link: '#',
+    },
+    {
+      name: 'Passport',
+      link: '#',
+    },
+  ]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const Upload = async () => {
+      axiosInstance
+        .post('/documents')
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data);
+          const { fileName, filePath, docs } = data;
+          setUploadedFile({ filePath, fileName });
+          setLoad(true);
+          setDocuments(docs);
+        });
+    };
+    Upload();
+  };
+
   return (
     <div
       style={{
@@ -32,81 +69,118 @@ function Dashboard() {
       <Navbar />
 
       <Grid container>
-      <Grid
-          item
-          xs={12}
-          sm={6}
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh',
-          }}
-        >
-          <Card sx={{
-            height: 400, width: 600, bgcolor: 'background.paper',
-            boxShadow: 7,
-            borderRadius: 3,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-            <CardContent >
-              <Typography variant="h4" color="text" gutterBottom sx={{ mt: 5, fontWeight: 700}}>
-                Upload Image
-              </Typography>
-            </CardContent>
-            <CardActions sx={{mt:10}}>
-              <Stack direction="row" alignItems="center" spacing={2} >
-                <label htmlFor="icon-button-file" >
-                  <Typography color="text.secondary" variant="h6">
-                  select the image:
-                  </Typography>
-                  <Input accept="image/*" id="icon-button-file" type="file" />
-                  <IconButton color="primary" aria-label="upload picture" component="span">
-                    <PhotoCamera sx={{ height:80, width: 68, alignItems: 'center', ml:3.5,
-            justifyContent: 'center',}} />
-                  </IconButton>
-                </label>
-              </Stack>
-            </CardActions>
-          </Card>
-        </Grid>
         <Grid
           item
           xs={12}
-          sm={6}
+          sm={12}
           sx={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
             justifyContent: 'center',
-            height: '100vh',
-            backgroundColor: 'white',
           }}
         >
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              color: 'black',
-              height: '100vh',
-              mt: 3.7
+          <Card
+            sx={{
+              boxShadow: 7,
+              borderRadius: 3,
+              padding: 3,
+              width: '80%',
+              margin: 20,
+              textAlign: 'center',
+              backgroundColor: '#fbfbfb',
             }}
+            elevation={10}
           >
-            <img
-              src={camera}
-              alt='camera'
-              style={{ width: '100%', height:'85%' }}
-              className='img-anim'
-            />
-          </div>
+            <CardContent></CardContent>
+            <CardActions
+              sx={{
+                fontWeight: 700,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <form
+                onSubmit={handleSubmit}
+                enctype='multipart/form-data'
+                className='form-container'
+              >
+                <label>
+                  <Typography
+                    variant='h4'
+                    color='text'
+                    style={{ marginBottom: '15px', fontWeight: 900 }}
+                  >
+                    Upload
+                    <span style={{ fontWeight: 300, fontFamily: 'Roboto' }}>
+                      Image
+                    </span>
+                  </Typography>
+                  <input
+                    accept='image/*'
+                    id='icon-button-file'
+                    type='file'
+                    className='img-uploader'
+                    onChange={(event) => setFile(event.target.files[0])}
+                  />
+                </label>
+              </form>
+              {load && (
+                <TableContainer
+                  component={Paper}
+                  sx={{
+                    margin: 10,
+                    borderRadius: 4,
+                  }}
+                  elevation={5}
+                >
+                  <Table aria-label='simple table'>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell
+                          colSpan={12}
+                          align='center'
+                          style={{ fontWeight: 700, fontSize: '18pt' }}
+                        >
+                          Documents
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {documents.map((document) => (
+                        <TableRow>
+                          <TableCell align='center'>{document.name}</TableCell>
+                          <TableCell align='center'>
+                            {' '}
+                            <a
+                              className='link-btn'
+                              target='_blank'
+                              href={document.link}
+                            >
+                              Open
+                            </a>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              )}
+              {!load && (
+                <img
+                  src={camera}
+                  alt='camera'
+                  style={{ width: '30%' }}
+                  className='img-anim'
+                />
+              )}
+            </CardActions>
+          </Card>
         </Grid>
       </Grid>
-    </div >
+    </div>
   );
 }
 
